f9e089802e89c877bcb43c3882e5d75af5657c61
Andrew Nelson <51036125+armyrunner@users.noreply.github.com>
2021-11-10 16:45:52 -0700

    Add files via upload

---
+++ GoPractice/api.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func PostMethod(c *gin.Context){
+	fmt.Println("\napi.go 'PostMethod' called")
+	message := "PostMethod called"
+	c.JSON(http.StatusOK, message)
+}
+
+func GetMethod(c *gin.Context){
+	fmt.Println("\napi.go 'GetMethod' called")
+	message := "GetMethod called"
+	c.JSON(http.StatusOK, message)
+}
+
+func PutMethod(c *gin.Context){
+	fmt.Println("\napi.go 'PutMethod' called")
+	message := "PutMethod called"
+	c.JSON(http.StatusOK, message)
+}
+
+func main(){
+	router :=gin.Default()
+
+	router.POST("/", PostMethod)
+	router.GET("/",GetMethod)
+	router.PUT("/",PutMethod)
+
+	listenPort := "8080"
+	router.Run(":"+listenPort)
+}
