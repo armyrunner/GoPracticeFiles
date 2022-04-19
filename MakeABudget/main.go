6886d9d1798d66ad25e2dd951844e7d563be5c9c
armyrunner <live2runmarathon@gmail.com>
2022-04-18 20:15:31 -0600

    WIP

--- MakeABudget/main.go
+++ MakeABudget/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	m2 "personal-budget/budgetProgram"
+	"os"
+
+	// bp "personal-budget/budgetProgram"
 	"time"
 )
 
@@ -19,38 +21,91 @@ var months = []time.Month{
 	time.October,
 	time.November,
 	time.December,
-	time.January,
+}
+
+func createBudgetMenu() {
+	i := 1
+	fmt.Println("Which month do you wnat to create a budget for?")
+
+	for _, month := range months{
+		fmt.Printf("%v. %v\n",i , month )
+		i++
+	}
+}
+
+func transactionMenu(){
+	fmt.Println("What do you want to do?")
+}
+
+func printBudgetMenu(){
+	fmt.Println("What do you want to print?")
+}
+
+func printMenu() {
+
+	menu := "1) Create A Budget\n" +
+			"2) Transactions\n" +
+			"3) Print Budget\n" +
+			"4) Exit\n "
+
+	fmt.Print(menu)
 }
 
 func main() {
-	bu, _ := m2.CreateBudget(time.January, 1000)
-	bu.AddItem("bananas", 10.0)
 
-	fmt.Println("Items in January:", len(bu.Items))
-	fmt.Printf("Current cost for January: $%.2f \n", bu.CurrentCost())
+	fmt.Println( "Please make a selection!")
+	fmt.Println(" ")
+	choice := -1
+	
+	printMenu()
+	fmt.Scanln(&choice)
 
-	m2.CreateBudget(time.February, 1000)
-
-	bu2 := m2.GetBudget(time.February)
-	bu2.AddItem("bananas", 10.0)
-	bu2.AddItem("coffee", 3.99)
-	bu2.AddItem("gym", 50.0)
-	bu2.RemoveItem("coffee")
-	fmt.Println("Items in February:", len(bu2.Items))
-	fmt.Printf("Current cost for February: $%.2f \n", bu2.CurrentCost())
-
-	fmt.Println("Resetting Budget Report...")
-	m2.InitializeReport()
-
-	for _, month := range months {
-		_, err := m2.CreateBudget(month, 100.00)
-		if err == nil {
-			fmt.Println("Budget created for", month)
-		} else {
-			fmt.Println("Error creating budget:", err)
+	for choice != 4 {
+		switch choice {
+		case 1:
+			createBudgetMenu()
+		case 2:
+			transactionMenu()
+		case 3:
+			printBudgetMenu()
+		default:
+			fmt.Println("Invalid Choice, Please try agian.")
 		}
+		printMenu()
+		fmt.Scanln(&choice)
+	
 	}
+	os.Exit(3)
 
-	_, err := m2.CreateBudget(time.December, 100.00)
-	fmt.Println(err)
+
+	// bu, _ := bp.CreateBudget(time.January, 1000)
+	// bu.AddItem("bananas", 10.0)
+
+	// fmt.Println("Items in January:", len(bu.Items))
+	// fmt.Printf("Current cost for January: $%.2f \n", bu.CurrentCost())
+
+	// bp.CreateBudget(time.February, 1000)
+
+	// bu2 := bp.GetBudget(time.February)
+	// bu2.AddItem("bananas", 10.0)
+	// bu2.AddItem("coffee", 3.99)
+	// bu2.AddItem("gym", 50.0)
+	// bu2.RemoveItem("coffee")
+	// fmt.Println("Items in February:", len(bu2.Items))
+	// fmt.Printf("Current cost for February: $%.2f \n", bu2.CurrentCost())
+
+	// fmt.Println("Resetting Budget Report...")
+	// bp.InitializeReport()
+
+	// for _, month := range months {
+	// 	_, err := bp.CreateBudget(month, 100.00)
+	// 	if err == nil {
+	// 		fmt.Println("Budget created for", month)
+	// 	} else {
+	// 		fmt.Println("Error creating budget:", err)
+	// 	}
+	// }
+
+	// _, err := bp.CreateBudget(time.December, 100.00)
+	// fmt.Println(err)
 }
